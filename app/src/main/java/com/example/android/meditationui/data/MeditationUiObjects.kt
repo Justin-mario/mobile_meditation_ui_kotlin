6d86803fac052dd75d07c0cc131f3ad91f333818
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-07-03 16:08:19 +0100

    done with feature section

--- app/src/main/java/com/example/android/meditationui/data/MeditationUiObjects.kt
+++ app/src/main/java/com/example/android/meditationui/data/MeditationUiObjects.kt
@@ -1,11 +1,55 @@
 package com.example.android.meditationui.data
 
+import com.example.android.meditationui.R
+import com.example.android.meditationui.ui.theme.Beige1
+import com.example.android.meditationui.ui.theme.Beige2
+import com.example.android.meditationui.ui.theme.Beige3
+import com.example.android.meditationui.ui.theme.BlueViolet1
+import com.example.android.meditationui.ui.theme.BlueViolet2
+import com.example.android.meditationui.ui.theme.BlueViolet3
+import com.example.android.meditationui.ui.theme.LightGreen1
+import com.example.android.meditationui.ui.theme.LightGreen2
+import com.example.android.meditationui.ui.theme.LightGreen3
+import com.example.android.meditationui.ui.theme.OrangeYellow1
+import com.example.android.meditationui.ui.theme.OrangeYellow2
+import com.example.android.meditationui.ui.theme.OrangeYellow3
+
 object MeditationUiObjects {
     val chips = listOf(
         "Sweet sleep",
         "Insomnia",
         "Depression",
         "Excitement",
-        "Prayer"
+        "Prayer")
+
+    val features = listOf(
+        Feature(
+            title = "Sleep meditation",
+            R.drawable.ic_headphone,
+            BlueViolet1,
+            BlueViolet2,
+            BlueViolet3
+        ),
+        Feature(
+            title = "Tips for sleeping",
+            R.drawable.ic_videocam,
+            LightGreen1,
+            LightGreen2,
+            LightGreen3
+        ),
+        Feature(
+            title = "Night island",
+            R.drawable.ic_headphone,
+            OrangeYellow1,
+            OrangeYellow2,
+            OrangeYellow3
+        ),
+        Feature(
+            title = "Calming sounds",
+            R.drawable.ic_headphone,
+            Beige1,
+            Beige2,
+            Beige3
+        )
     )
 }
